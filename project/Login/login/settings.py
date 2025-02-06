ca293cf7927f5f8f0edb773b1e76f00a7ef93e6c
willi.weihrich <willi.weihrich@kks.kbs.schule>
2025-02-06 12:08:15 +0100

    Prepare App for Docker Env Variables

--- project/Login/login/settings.py
+++ project/Login/login/settings.py
@@ -21,15 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-&#&y(d9$o)l5nzpz=_ru&7mje8!e=zg)pfp@k-%(+l%0tw+iqx'
+#SECRET_KEY = os.environ.get('SECRET_KEY') Using Docker OV environment variable
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+#DEBUG = os.environ.get('DEBUG') Using Docker OV environment variable
 
 ALLOWED_HOSTS = [
     '172.30.2.104',
     '127.0.0.1',
     'localhost'
 ]
+#ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',') Using Docker OV environment variable
 
 
 # Application definition
